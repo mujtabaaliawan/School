ce91bfdd3d0e673816d69b807925c5da3b18d641
mujtabaaliawan <117171332+mujtabaaliawan@users.noreply.github.com>
2022-11-21 15:20:14 +0500

    middleware updated

---
+++ school_management/middlewares/request.py
@@ -0,0 +1,28 @@
+from django.utils.deprecation import MiddlewareMixin
+
+
+class RequestRoleMiddleware(MiddlewareMixin):
+
+    def process_request(self, request):
+
+        if request.user.is_authenticated:
+            request.teacher = None
+            request.student = None
+            request.staff = None
+            if request.user.is_teacher:
+                request.teacher = request.user
+            elif request.user.is_student:
+                request.student = request.user
+            elif request.user.is_admin:
+                request.staff = request.user
+
+        return request
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        request = self.process_request(request)
+        response = self.get_response(request)
+        return response
+
--- school_management/settings.py
+++ school_management/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'school_management.greet_middleware.Greet',
+    'school_management.middlewares.request.RequestRoleMiddleware',
 ]
 
 ROOT_URLCONF = 'school_management.urls'
